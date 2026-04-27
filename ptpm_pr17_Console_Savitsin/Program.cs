1e49712b46cdd12e28783a4bf5b0f17e91996776
AndreiSavitsin <andreeeeewcs@gmail.com>
2026-04-27 14:06:38 +0500

    Add files via upload

---
+++ ptpm_pr17_Console_Savitsin/Program.cs
@@ -0,0 +1,22 @@
+﻿using ptpm_pr17_Savitsin;
+
+StudentsClass student = new StudentsClass();
+
+Console.Write("Введите фамилию: ");
+student.Surname = Console.ReadLine();
+
+Console.Write("Введите специальность: ");
+student.Speciality = Console.ReadLine();
+
+Console.Write("Введите курс: ");
+if (int.TryParse(Console.ReadLine(), out int course))
+{
+    student.Course = course;
+}
+
+Console.Write("Очное или заочное обучение: ");
+student.FullOrPart = Console.ReadLine();
+
+Console.WriteLine(StudentsClass.GetInfo(student));
+
+Console.ReadKey();
---
+++ ptpm_pr17_Savitsin/StudentsClass.cs
@@ -0,0 +1,24 @@
+﻿namespace ptpm_pr17_Savitsin
+{
+    public class StudentsClass //Базовый класс
+    {
+        static List<StudentsClass> studentsList = new List<StudentsClass>();
+
+        string surname; //Фамилия
+        string speciality; //Специальность
+        int course; //Курс
+        string fullOrPart; //Очник или заочник
+
+        public string Surname { get; set; }
+        public string Speciality { get; set; }
+        public int Course { get; set; }
+        public string FullOrPart { get; set; }
+        public StudentsClass() { }
+
+        static public string GetInfo(StudentsClass student)
+        {
+            return $"Фамилия: {student.Surname}. Специальность: {student.Speciality}. " +
+                $"Курс: {student.Course}. Очник или заочник: {student.FullOrPart}";
+        }
+    }
+}
